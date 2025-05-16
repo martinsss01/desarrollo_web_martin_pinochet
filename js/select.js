9935949876019b08b41d513472eaa8ec6eb01927
martinsss01 <martin.pinochet@ug.uchile.cl>
2025-05-15 23:17:31 -0400

    fix: almost there...

--- js/select.js
+++ js/select.js
@@ -1,36 +1,54 @@
 const data = {
-    "850": ["F10", "F11", "F21", "QP", "QO", "Q10", "G301", "G303", "G101"],
-    "851": ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B010"]
-}
+  "850": ["G303", "G304", "F10", "F11", "QP", "QO"],
+  "851": ["B01", "B02", "B03", "B04", "B05", "B06"]
+};
 
-const classRoomFilter = () => {
-    let areaSelect = document.getElementById("area");
-    for (const area in data) {
-
+const poblarSalas = () => {
+    let departmentSelect = document.getElementById("select-area");
+    for (const department in data) {
         let option = document.createElement("option");
-        option.value = area;
-        option.text = area;
-        areaSelect.append(option);
+        option.value = department;
+        option.text = department;
+        departmentSelect.appendChild(option);
     }
-}
+};
 
-const update = () =>{ 
-    let areaSelect = document.getElementById("area");
-    let classroomSelect = document.getElementById("classroom");
-    let selectedArea = areaSelect.value;
+const updateCursos = () => {
+    let departmentSelect = document.getElementById("select-area");
+    let courseSelect = document.getElementById("select-classroom");
+    let selectedDepartment = departmentSelect.value;
 
-    if (data[selectedArea]){
-        data[selectedArea].forEach(classroom => {
+    courseSelect.innerHTML = '<option value="">Seleccione una sala</option>';
+
+    if (data[selectedDepartment]) {
+        data[selectedDepartment].forEach(course => {
             let option = document.createElement("option");
-            option.value = classroom;
-            option.text = classroom;
-            classroomSelect.appendChild(option);
-        })
+            option.value = course;
+            option.text = course;
+            courseSelect.appendChild(option);
+        });
     }
+  changeArguments();
+};
+
+function changeArguments() {
+  const courseSelect = document.getElementById("select-area");
+  const reasonLabel = document.querySelector("label[for='reason']");
+  const reasonTextarea = document.getElementById("comments");
+  
+  if (courseSelect.value !== "") {
+      reasonLabel.style.display = "block";
+      reasonTextarea.style.display = "block";
+  } else {
+      reasonLabel.style.display = "none";
+      reasonTextarea.style.display = "none";
+  }
 }
 
-document.getElementById("area").addEventListener("change", update);
+document.getElementById("select-area").addEventListener("change", updateCursos);
+document.getElementById("select-classroom").addEventListener("change", changeArguments);
 
 window.onload = () => {
-    classRoomFilter();
-}
+  poblarSalas();
+  changeArguments();
+};
